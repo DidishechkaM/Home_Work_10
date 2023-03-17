c732c0b1972fbd0cc4193191b7e0bbf103d01104
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-03-17 14:28:48 +0300

    added new

---
+++ src/test/java/ru/maksimova/SearchButtonLogin.java
@@ -0,0 +1,38 @@
+package ru.maksimova;
+
+
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static java.lang.Thread.sleep;
+
+
+public class SearchButtonLogin {
+    @DisplayName("Проверка работы сайта smetter.ru")
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+    @BeforeEach
+    void beforeEach () {
+    Selenide.open("https://smetter.ru/");
+}
+
+
+   @Test
+    void searchButtonLog() {
+        Selenide.$("a.tn-atom").$(byText("Войти")).click();
+
+
+
+
+    }
+}
