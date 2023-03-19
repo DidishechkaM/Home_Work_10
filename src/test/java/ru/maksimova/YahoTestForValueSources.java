b08e8a732c6c3ed2ef8ef8d5882c9154b93d0f24
Diana Maksimowa <dianamaksimowa@yandex.ru>
2023-03-19 11:36:47 +0300

    full test with annotations

--- src/test/java/ru/maksimova/YahoTestForValueSources.java
+++ src/test/java/ru/maksimova/YahoTestForValueSources.java
@@ -5,14 +5,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selenide.*;
 
 @DisplayName("Проверка работы сайта YAHOO.com")
-public class YahoTestForValueSource {
+public class YahoTestForValueSources {
     @BeforeEach
     void openSite() {
         Configuration.browserSize = "1920x1080";
@@ -30,7 +29,7 @@ public class YahoTestForValueSource {
     void  searhResultForYahoo( String TestData) {
         $("#ybar-search-box-container").click();
         $("input[type='text']").setValue(TestData).pressEnter();
-        $$("div.dd.algo").shouldHave(CollectionCondition.sizeGreaterThanOrEqual(5));
+        $$("div.dd.algo").shouldHave(CollectionCondition.sizeGreaterThanOrEqual(4));
 
 
     }
